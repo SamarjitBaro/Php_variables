a8da9aab041ff1cce8d0561f96a219e15d3b622a
Samarjit Baro <xamarbro10@gmail.com>
2024-11-29 21:16:28 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+$nam = "samarjit baro";
+$food = "Green veg";
+$quantity = 30;
+$price = 4.99;
+echo "hello {$nam} <br>";
+echo "You like {$food}<br>";
+echo "Yoi ordered {$quantity} of {$food}<br>";
+$total = $quantity * $price;
+echo "your total price is \${$total}";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Hello</button>
+    <button>sad</button>
+</body>
+</html>
